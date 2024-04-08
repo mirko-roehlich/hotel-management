ecd3b0d4b259538bed8e02e74c2f29fe16383865
mirko-roehlich <github@streetjourney.de>
2024-04-08 23:17:55 +0200

    stronlgy typed hotel

--- src/HotelManagement.Api/Data/Models/Hotel.cs
+++ src/HotelManagement.Api/Data/Models/Hotel.cs
@@ -2,7 +2,13 @@ namespace HotelManagement.Api.Data.Models;
 
 public class Hotel
 {
-    public int Id { get; set; }
+    public HotelId Id { get; set; }
     public string Name { get; set; } = string.Empty;
     public IList<Room> Rooms { get; set; } = [];
+}
+
+public readonly record struct HotelId(int Value)
+{
+    public static implicit operator int(HotelId hotelId) => hotelId.Value;
+    public override string ToString() => Value.ToString();
 }
