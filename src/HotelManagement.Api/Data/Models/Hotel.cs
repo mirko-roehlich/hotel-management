d03a6189a51f78d192bdf844bd2e13f3eb394c60
mirko-roehlich <github@streetjourney.de>
2024-04-14 13:54:50 +0200

    first iterration of hotel with encapsulation

--- src/HotelManagement.Api/Data/Models/Hotel.cs
+++ src/HotelManagement.Api/Data/Models/Hotel.cs
@@ -2,13 +2,31 @@ namespace HotelManagement.Api.Data.Models;
 
 public class Hotel
 {
-    public HotelId Id { get; set; }
-    public string Name { get; set; } = string.Empty;
-    public IList<Room> Rooms { get; set; } = [];
+    public HotelId Id { get; private set; }
+    public string Name { get; private set; }
+    private readonly List<Room> _rooms;
+    public IReadOnlyCollection<Room> Rooms => _rooms.AsReadOnly();
+
+    public Hotel(string name)
+    {
+        ArgumentException.ThrowIfNullOrWhiteSpace(name);
+
+        Id = HotelId.Empty;
+        Name = name;
+        _rooms = [];
+    }
+
+    public void UpdateHotelName(string name)
+    {
+        ArgumentException.ThrowIfNullOrWhiteSpace(name);
+
+        Name = name;
+    }
 }
 
 public readonly record struct HotelId(int Value)
 {
+    public static HotelId Empty => new(0);
     public static implicit operator int(HotelId hotelId) => hotelId.Value;
     public override string ToString() => Value.ToString();
 }
